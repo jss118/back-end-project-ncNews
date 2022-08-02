e654dd3685177551195b7b9c51427add279e9917
jss118 <joesmith_guitar@hotmail.co.uk>
2022-08-02 12:22:23 +0100

    add getArticleById and tests

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { getTopics } = require("./controllers/app.controller");
+const { getTopics, getArticleById } = require("./controllers/app.controller");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Sorry, request invalid.." });
--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,5 +1,13 @@
-const { fetchTopics } = require("../models/app.model");
+const { fetchTopics, fetchArticle } = require("../models/app.model");
 
-exports.getTopics = (req, res, next) => {
+exports.getTopics = (req, res) => {
   fetchTopics().then(topics => res.send(topics));
+};
+
+exports.getArticleById = (req, res, next) => {
+  const id = req.params.article_id;
+
+  fetchArticle(id)
+    .then(([article]) => res.send(article))
+    .catch(next);
 };
