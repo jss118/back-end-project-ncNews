27d13b09ef296af32af8f25e2dd07bfc2f278909
jss118 <joesmith_guitar@hotmail.co.uk>
2022-08-22 09:58:13 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   getTopics,
   getArticleById,
@@ -12,6 +13,8 @@ const {
 } = require("./controllers/app.controller");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
